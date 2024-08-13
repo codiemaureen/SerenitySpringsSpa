a2202af00708dbb9f2641fc30cea99e27d0afbf2
Codiemaureen <codiestover.cs@gmail.com>
2024-08-13 15:07:18 -0400

    Adding Tab title and logo

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/22992/stones">Stones</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
+    </div>
+  )
+}
+
+export default Footer
