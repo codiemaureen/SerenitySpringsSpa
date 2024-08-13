056812cecba20f2e8fceab4f839fb68197bb825a
Codiemaureen <codiestover.cs@gmail.com>
2024-08-13 15:54:15 -0400

    Styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div>
+    <div className="footer">
       <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/22992/stones">Stones</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
     </div>
   )
