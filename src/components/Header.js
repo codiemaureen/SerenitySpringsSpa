b4185118028818012daa850d90aa1320059cd1a7
Codiemaureen <codiestover.cs@gmail.com>
2025-02-19 13:18:23 -0500

    Rebuilding header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import headerImg from "../images/SerenitySpringsHeader.jpg";
+import '../style/header.css';
+
+const Header = () => {
+  return (
+    <section>
+      <h1>Serenity Spa</h1>
+    </section>
+  )
+}
+
+export default Header
