edbcc4f2bf9d18e9af39f2c1abd9d780ee05dfca
Codiemaureen <codiestover.cs@gmail.com>
2025-02-20 14:10:52 -0500

    Adding about us section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import AboutUs from "./components/AboutUs";
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutUs.js
@@ -0,0 +1,16 @@
+import aboutUsImg from '../images/hair.jpg'
+import '../style/aboutUs.css';
+
+const AboutUs = () => {
+  return (
+   <div id='aboutContainer'>
+    <div className='about'>
+      <h2>About Us</h2>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus voluptate sed laboriosam nemo hic cum blanditiis nihil impedit facere aspernatur?</p>
+    </div>
+     <img src={aboutUsImg} alt="aboutUsImg"/>
+   </div>
+  )
+}
+
+export default AboutUs
