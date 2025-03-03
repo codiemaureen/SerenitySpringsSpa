81cdd041fbe0cb61914aedbcae770abe79ca8d9f
Codiemaureen <codiestover.cs@gmail.com>
2025-03-03 14:54:26 -0500

    adding serivce links component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import AboutUs from "./components/AboutUs";
+import Servicelinks from "./components/Servicelinks";
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Servicelinks />
       <AboutUs />
     </div>
   );
---
+++ src/components/Servicelinks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style/serviceLinks.css';
+
+const Servicelinks = () => {
+  return (
+    <div className='flex-container'>
+      <img src='' alt='service-icons' className='flex-items'/>
+      <h2 className='flex-items'>Title</h2>
+      <p className='flex-items'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum a sint veritatis incidunt doloremque autem quibusdam vel maxime quaerat necessitatibus aliquid explicabo porro doloribus provident, atque inventore facere unde at?</p>
+    </div>
+  )
+}
+
+export default Servicelinks
