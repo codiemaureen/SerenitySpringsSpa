6461d8e8b43a0841bf92b0ce36c176951f49eb7f
Codiemaureen <codiestover.cs@gmail.com>
2025-03-04 17:39:02 -0500

    Reformatting ServiceLink to indv link component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from "./components/Header";
 import AboutUs from "./components/AboutUs";
-import Servicelinks from "./components/Servicelinks";
+import Servicelink from "./components/Servicelink";
+import aboutUsImg from '../src/images/hair.jpg'
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Servicelinks />
+      <Servicelink image={aboutUsImg}/>
       <AboutUs />
     </div>
   );
--- src/components/Servicelink.jsx
+++ src/components/Servicelink.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import '../style/serviceLinks.css';
+import '../style/serviceLink.css';
 
-const Servicelinks = () => {
+const Servicelink = ({image}) => {
   return (
     <div className='flex-container'>
-      <img src='' alt='service-icons' className='flex-items'/>
+      <img src={image} alt='service-icons' className='flex-items'/>
       <h2 className='flex-items'>Title</h2>
       <p className='flex-items'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum a sint veritatis incidunt doloremque autem quibusdam vel maxime quaerat necessitatibus aliquid explicabo porro doloribus provident, atque inventore facere unde at?</p>
     </div>
   )
 }
 
-export default Servicelinks
+export default Servicelink
