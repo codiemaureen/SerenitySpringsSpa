a574ff9a5fdbb5ff71b5c64125d7eb5b3573e929
Codiemaureen <codiestover.cs@gmail.com>
2025-04-03 18:30:54 -0400

    Adding static service data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import Header from "./components/Header";
 import AboutUs from "./components/AboutUs";
 import Servicelink from "./components/Servicelink";
-import aboutUsImg from '../src/images/hair.jpg'
 import './index.css';
+import { useState } from "react";
+import aboutUsImg from '../src/images/hair.jpg'
 
 function App() {
+  const [services, setServices] = useState([
+      {
+            id: 1,
+            title: "Hair",
+            image: aboutUsImg,
+            description: "We do hair"
+      },
+        {
+            id: 1,
+            title: "Makeup",
+            image: aboutUsImg,
+            description: "we do makeup"
+      },
+        {
+            id: 1,
+            title: "Nails",
+            image: aboutUsImg,
+            description: "we do nails"
+      }
+  ]);
+
   return (
     <div className="App">
       <Header />
-      <Servicelink image={aboutUsImg}/>
+      <Servicelink services={services}/>
       <AboutUs />
     </div>
   );
