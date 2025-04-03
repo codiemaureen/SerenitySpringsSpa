66090c159faa8a4af014c807f4d276cf89df50f4
Codiemaureen <codiestover.cs@gmail.com>
2025-04-03 18:48:54 -0400

    Creating parent service component

--- src/components/Servicelink.jsx
+++ src/components/Servicelink.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../style/serviceLink.css';
+import Service from './Service';
 
-const Servicelink = ({image}) => {
+
+const Servicelink = ({services}) => {
   return (
-    <div className='flex-container'>
-      <img src={image} alt='service-icons' className='flex-items'/>
-      <h2 className='flex-items'>Title</h2>
-      <p className='flex-items'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum a sint veritatis incidunt doloremque autem quibusdam vel maxime quaerat necessitatibus aliquid explicabo porro doloribus provident, atque inventore facere unde at?</p>
+    <div className='services-container'>
+      <h2 className='header'>Take a Look at our Services</h2>
+      <div className='services'>
+      {services.map((service)=> (
+        <Service service={service} />
+      ))}
+      </div>
+
     </div>
   )
 }
