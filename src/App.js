d698575b85a013d568fb991448b1128d7c249b74
Codiemaureen <codiestover.cs@gmail.com>
2025-04-03 19:09:55 -0400

    Adding footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Servicelink from "./components/Servicelink";
 import './index.css';
 import { useState } from "react";
 import aboutUsImg from '../src/images/hair.jpg'
+import Footer from "./components/Footer";
 
 function App() {
   const [services, setServices] = useState([
@@ -32,6 +33,7 @@ function App() {
       <Header />
       <Servicelink services={services}/>
       <AboutUs />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../style/footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-container" id="footer-container">
+      <h4>WebSite Built by <a href="https://codiemaureen.netlify.app/" target="_blank">Codie Groth</a></h4>
+    </div>
+  )
+}
+
+export default Footer
