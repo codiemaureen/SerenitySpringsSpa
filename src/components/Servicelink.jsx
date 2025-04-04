3f4e68b31564bbe0680c8cac67e79c42d51fd08a
Codiemaureen <codiestover.cs@gmail.com>
2025-04-04 11:26:52 -0400

    Adjusting services  mobile view

--- src/components/Servicelink.jsx
+++ src/components/Servicelink.jsx
@@ -6,13 +6,12 @@ import Service from './Service';
 const Servicelink = ({services}) => {
   return (
     <div className='services-container'>
-      <h2 className='header'>Take a Look at our Services</h2>
+      <h2 className='services-header'>Take a Look at our Services</h2>
       <div className='services'>
-      {services.map((service)=> (
-        <Service service={service} />
-      ))}
+        {services.map((service)=> (
+          <Service service={service} />
+        ))}
       </div>
-
     </div>
   )
 }
