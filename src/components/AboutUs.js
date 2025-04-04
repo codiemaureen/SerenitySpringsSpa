f1a42d698ce79fc24d31e530f14a53815c684319
Codiemaureen <codiestover.cs@gmail.com>
2025-04-04 11:24:34 -0400

    Adjusting about me mobile view

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,8 +4,8 @@ import '../style/aboutUs.css';
 const AboutUs = () => {
   return (
    <div id='aboutContainer'>
-    <div className='about'>
-      <h2>About Us</h2>
+    <div className='about-desc'>
+      <h2 className='about-header'>About Us</h2>
       <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus voluptate sed laboriosam nemo hic cum blanditiis nihil impedit facere aspernatur?</p>
     </div>
      <img src={aboutUsImg} alt="aboutUsImg" className='about-img'/>
