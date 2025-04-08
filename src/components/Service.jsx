33a02493e984969cff02852f3c026ad5b54262c4
Codiemaureen <codiestover.cs@gmail.com>
2025-04-08 19:43:41 -0400

    Style: working on image title and description porportions

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,7 +4,7 @@ import '../style/services.css';
 const Service = ({service}) => {
   return (
       <div className='service-container'>
-          <h1>{service.title}</h1>
+          <h1 className='service-title'>{service.title}</h1>
           <img src={service.image} alt={service.title} className='service-image'/>
           <p className='service-description'>{service.description}</p>
           <a href='' target='_blank' className='book-btn'>Book Now!</a>
