9ee67835d8f940f8b8b65d5206e37f5a4fd30988
Codiemaureen <codiestover.cs@gmail.com>
2025-04-08 19:33:39 -0400

    Feat: adding new book now button

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,11 +1,14 @@
 import '../style/services.css';
 
+
 const Service = ({service}) => {
   return (
       <div className='service-container'>
           <h1>{service.title}</h1>
           <img src={service.image} alt={service.title} className='service-image'/>
-          <p>{service.description}</p>
+          <p className='service-description'>{service.description}</p>
+          <a href='' target='_blank' className='book-btn'>Book Now!</a>
+
       </div>
   )
 }
