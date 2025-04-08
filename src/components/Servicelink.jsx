5be91d6747826361a4b58ecf5c50d7aaadb9dd18
Codiemaureen <codiestover.cs@gmail.com>
2025-04-08 19:54:27 -0400

    Style: fixing header

--- src/components/Servicelink.jsx
+++ src/components/Servicelink.jsx
@@ -6,7 +6,7 @@ import Service from './Service';
 const Servicelink = ({services}) => {
   return (
     <div className='services-container'>
-      <h2 className='services-header'>Take a Look at our Services</h2>
+      <h2 className='services-header'>Our Services</h2>
       <div className='services'>
         {services.map((service)=> (
           <Service service={service} />
