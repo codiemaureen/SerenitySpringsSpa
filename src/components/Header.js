9111c8a8cdf4fc87bd326e100250a009d9aa8230
Codiemaureen <codiestover.cs@gmail.com>
2025-04-17 17:40:18 -0400

    Style: adjusting mobile and desktop view and text size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
-import NavBar from './NavBar';
 import '../style/header.css';
 
 const Header = () => {
   return (
     <section id='header'>
-     <NavBar/>
       <h1 className='page-header'>Serenity Springs Spa</h1>
     </section>
   )
