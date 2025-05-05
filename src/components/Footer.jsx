8c61a3bf7ad2b1081fd737801723a65a7af76e6a
Codiemaureen <codiestover.cs@gmail.com>
2025-05-05 12:07:33 -0400

    fixing url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../style/footer.css';
 const Footer = () => {
   return (
     <div className="footer-container" id="footer-container">
-      <h4>WebSite Built by <a href="https://codiemaureen.netlify.app/" target="_blank">Codie Groth</a></h4>
+      <h4>WebSite Built by <a href="https://codiegroth.com/" target="_blank">Codie Groth</a></h4>
     </div>
   )
 }
