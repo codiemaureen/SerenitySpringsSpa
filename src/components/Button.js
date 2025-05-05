13d48b2a93871f79a328e50cdaf677d3c13d66a3
Codiemaureen <codiestover.cs@gmail.com>
2025-05-05 09:10:24 -0400

    Adding title to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ const Button = ({ icon, title }) => {
   return (
    <>
     <a href={`/${title}`} target="_blank">
-     <button type="submit" className="btn">{icon}</button>
+     <button type="submit" className="btn">{icon}{title}</button>
     </a>
    </>
   )
