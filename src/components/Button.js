638e111f85e727057f284e3e1c67aaad48845e43
Codiemaureen <codiestover.cs@gmail.com>
2025-05-05 13:56:12 -0400

    Feature: Adding hamburger menu button to navBar

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,9 @@ import "../style/button.css";
 const Button = ({ icon, title }) => {
 
   return (
-   <>
-    <a href={`/${title}`} target="_blank">
+
      <button type="submit" className="btn">{icon}{title}</button>
-    </a>
-   </>
+
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,36 @@
 import '../style/navBar.css'
 import Button from './Button';
 import { BsBag } from "react-icons/bs";
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 
 
 const NavBar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+
   let navItems = [
-    {title:'home', link: ''}, 
+    {title:'home', link: '/'}, 
     {title:'about', link:"#aboutContainer" } , 
     {title: 'services', link: "#servicesContainer" }, 
     {title: <Button icon={<BsBag color="white" />} />, link: null },
     {title: 'book online', link: null }
   ]
   return (
-   <div id='navBar'>
-    <nav>
-      {/* hide until header image is out of view */}
+   <div id='navBarContainer'>
+    <div className='titleDropdown'>
       <p className='headerTitle'><a href=''>Serenity Springs Spa</a></p>
-      {/* Add a menu dropdown for mobile */}
-      {navItems.map((items, i) => (
+      <button className='menuButton' onClick={() => setIsDropdownOpen((prev) => !prev)}><FaBars/></button>
+    </div>
+
+    {isDropdownOpen &&
+      <nav className='navBar'>
+        {navItems.map((items, i) => (
         
         <ol>
           <li><a href={items.link}>{items.title}</a></li>
         </ol>
       ))}
-    </nav>
+      </nav>}
    </div>
   )
 }
